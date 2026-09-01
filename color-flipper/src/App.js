dee2ad533445daa3980f59882b3c8f7fd670b023
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-09-01 16:01:19 +0530

    Add localStorage to save background color

--- color-flipper/src/App.js
+++ color-flipper/src/App.js
@@ -1,21 +1,34 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 
 function App(){
   const [color, setColor] = useState("rgb(128, 128, 128)")
 
+  useEffect(() => {
+    const savedColor = localStorage.getItem("bgColor");
+    if(savedColor){
+      setColor(savedColor);
+    }
+  })
+  
   const getRandomColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    return `rgb(${r},)`
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  const handleClick = () => {
+    const newColor = getRandomColor();
+    setColor(newColor)
+    localStorage.setItem("bgColor", newColor)
   }
 
   return (
     <div className="container" style={{background: color}}>
       <p className="colortext" >Background-color: {color} </p>
-      <button className="click-button">Click me</button>
+      <button className="click-button" onClick={handleClick}>Click me</button>
     </div>
   )
 }
