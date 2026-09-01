d2df7763d7c1fbee644e27139c4f17bdd74d496d
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-09-01 15:59:14 +0530

    generate random color

--- color-flipper/src/App.js
+++ color-flipper/src/App.js
@@ -1,12 +1,21 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
-function App(){
 
+function App(){
+  const [color, setColor] = useState("rgb(128, 128, 128)")
+
+  const getRandomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},)`
+  }
 
   return (
-    <div className="container">
-      <p className="colortext"></p>
-      <button className="click-button"></button>
+    <div className="container" style={{background: color}}>
+      <p className="colortext" >Background-color: {color} </p>
+      <button className="click-button">Click me</button>
     </div>
   )
 }
