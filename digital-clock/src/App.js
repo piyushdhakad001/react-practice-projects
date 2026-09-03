0551181da63b473cf530f6684e2db2afc7194e2c
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-09-03 13:23:07 +0530

    Display and feat: update time every second using useState and useEffect

--- digital-clock/src/App.js
+++ digital-clock/src/App.js
@@ -1,1 +1,27 @@
+import { useEffect, useState } from "react";
+import "./App.css"
+function App() {
 
+  const [time, setTime] = useState('00:00:00')
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      setTime(now.toLocaleTimeString());
+    }
+
+    const interval = setInterval(updateTime, 1000)
+    
+  }, [])
+
+
+
+
+  return (
+    <>
+      <h1 className="digital-clock" >Digital Clock</h1>
+      <p className="time">Current Time: {time}</p>
+    </>
+  )
+}
+export default App;
